285e9ec2a9bbde15c5ff7f67bf0b8543d713821f
MarvRoldan <marvinroldan_@hotmail.com>
2020-05-11 14:35:09 -0600

    Added get/set for earch field

--- Program.cs
+++ Program.cs
@@ -2,15 +2,65 @@
 
 namespace Painting
 {
-    class Painting {
+    class Painting
+    {
         private string name;
-        
+
         private string artist;
 
         private string medium;
 
         private int year;
+
+        public string Myname
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Myartist
+        {
+            get
+            {
+                return artist;
+            }
+            set
+            {
+                artist = value;
+            }
+        }
+
+        public string Mymedium
+        {
+            get
+            {
+                return medium;
+            }
+            set
+            {
+                medium = value;
+            }
+        }
+
+        public int Myyear
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+            }
+        }
     }
+
     class Program
     {
         static void Main(string[] args)
